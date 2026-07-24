4987353097a65fb0fa4ff00377e0c902611425cb
Stibik <grabinov2018@gmail.com>
2026-07-25 00:31:02 +0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -178,6 +178,7 @@ export function ensureSchema() {
   safeAddColumn("orders", "buy_price_cny REAL");
   safeAddColumn("orders", "buy_price_kzt REAL");
   safeAddColumn("orders", "delivery_price REAL");
+  safeAddColumn("orders", "entries_raw TEXT");
 
   const kaspiShopsRow = db.prepare("SELECT value FROM settings WHERE key = 'kaspi_shops'").get();
   if (!kaspiShopsRow) {
